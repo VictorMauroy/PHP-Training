6a104c6245dc0fc50f543ded8963d4293f88d4f8
VictorMauroy <mauroy.victor@gmail.com>
2024-07-12 17:55:31 +0200

    feat: add some interesting result, made by others

--- Kata/Level_6/end_with.php
+++ Kata/Level_6/end_with.php
@@ -6,3 +6,26 @@ function solution($str, $ending) {
   return (bool) $result;
 }
 
+/* Not by me
+
+    BEST SOLUTION:
+
+function solution(string $str, string $ending): bool {
+    return str_ends_with($str, $ending);
+}
+
+
+    The solution I tried to do:
+
+function solution($str, $ending): bool {
+    $inp = preg_quote($ending);
+    return !!preg_match("/{$inp}$(?!\n)/", $str);
+}
+
+    Without built-in function:
+
+function solution($str, $ending): bool {
+    return substr($str, -strlen($ending), strlen($ending)) == $ending;
+}
+
+*/
